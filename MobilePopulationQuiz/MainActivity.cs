75f6d013acdfc2c24a71cb1445a1da185d882330
Garrett <Garrettdeblois13@yahoo.com>
2017-10-18 16:16:37 -0500

    Completed: Made it so that users can’t select next button before selecting an answer. Fixed the issues with the random number generator not excluding already answered questions.
    In progress: I currently am working to fix the colors of the buttons as well as the score calculation at the bottom. Also, at the end of game when final number or question has been reached, need to rename the next button to finish and direct the user to an end screen with their score and a button that takes them back to a main menu

--- MobilePopulationQuiz/MainActivity.cs
+++ MobilePopulationQuiz/MainActivity.cs
@@ -22,19 +22,14 @@ namespace MobilePopulationQuiz
             Button btnNext = FindViewById<Button>(Resource.Id.button5);
             TextView question = FindViewById<TextView>(Resource.Id.textView1);
             TextView score = FindViewById<TextView>(Resource.Id.textView3);
+            btnNext.Enabled = false;
 
             b1.SetBackgroundColor(Android.Graphics.Color.DimGray);
             b2.SetBackgroundColor(Android.Graphics.Color.DimGray);
             b3.SetBackgroundColor(Android.Graphics.Color.DimGray);
             b4.SetBackgroundColor(Android.Graphics.Color.DimGray);
 
-            List<int> alreadyUsed = new List<int>();
-            alreadyUsed.Add(1);
-
-            if (alreadyUsed.Contains(1))
-            {
-                question.Text = "yes"; //testing 
-            }
+            List<int> alreadyUsed = new List<int>();//list for the numbers which have already been selected by the random number generator and therefore cannot be used again to select the next question
 
             void disableButtons()
             {
@@ -42,6 +37,7 @@ namespace MobilePopulationQuiz
                 b2.Enabled = false;
                 b3.Enabled = false;
                 b4.Enabled = false;
+                btnNext.Enabled = true;
             };
 
             void enableButtons()
@@ -50,6 +46,7 @@ namespace MobilePopulationQuiz
                 b2.Enabled = true;
                 b3.Enabled = true;
                 b4.Enabled = true;
+                btnNext.Enabled = false;
             };
 
             void calculateScore()
@@ -60,22 +57,21 @@ namespace MobilePopulationQuiz
 
             btnNext.Click += delegate
             {
-                System.Random RNG = new System.Random(System.DateTime.Now.Millisecond);
-                int randomNumber = RNG.Next(1, 3);
-
-                if (alreadyUsed.Contains(randomNumber)) //while that question number is already in the list, don't use that question
+                if (totalQuestions > 8) //this number needs to be one less than the total number of questions 
                 {
-                    btnNext.PerformClick(); //this is what is crashing the program currently
+                    btnNext.Text = "Finish";
+                    //insert code here to finish the app and take the user to a score page with main menu/restart buttons
                 }
-
-                int randomQuestion;
-
-                Toast.MakeText(this, randomNumber.ToString(), ToastLength.Short).Show(); //testing purposes, remove later
-                enableButtons();
-                b1.SetBackgroundColor(Android.Graphics.Color.DimGray);
-                b2.SetBackgroundColor(Android.Graphics.Color.DimGray);
-                b3.SetBackgroundColor(Android.Graphics.Color.DimGray);
-                b4.SetBackgroundColor(Android.Graphics.Color.DimGray);
+                b3.Visibility = Android.Views.ViewStates.Visible;
+                b4.Visibility = Android.Views.ViewStates.Visible;
+                System.Random RNG = new System.Random(System.DateTime.Now.Millisecond);
+                int randomNumber;
+                do
+                {
+                    randomNumber = RNG.Next(1, 10); //the second digit will always be 1 greater than the number of total questions
+                    Toast.MakeText(this, randomNumber.ToString(), ToastLength.Short).Show(); //testing purposes, remove later
+                }
+                while (alreadyUsed.Contains(randomNumber)); //if the already used question number comes up in the random number generator, generate a new random number and try again
 
                 switch (randomNumber)
                 {
@@ -85,7 +81,33 @@ namespace MobilePopulationQuiz
                     case 2:
                         questionTwo();
                         break;
-                }
+                    case 3:
+                        questionThree();
+                        break;
+                    case 4:
+                        questionFour();
+                        break;
+                    case 5:
+                        questionFive();
+                        break;
+                    case 6:
+                        questionSix();
+                        break;
+                    case 7:
+                        questionSeven();
+                        break;
+                    case 8:
+                        questionEight();
+                        break;
+                    case 9:
+                        questionNine();
+                        break;
+                }           
+                enableButtons();
+                b1.SetBackgroundColor(Android.Graphics.Color.DimGray);
+                b2.SetBackgroundColor(Android.Graphics.Color.DimGray);
+                b3.SetBackgroundColor(Android.Graphics.Color.DimGray);
+                b4.SetBackgroundColor(Android.Graphics.Color.DimGray);
             };
 
             btnNext.PerformClick(); //generates a random question the first time the program is run
@@ -189,11 +211,309 @@ namespace MobilePopulationQuiz
                     alreadyUsed.Add(2);
                 };
             }
+            void questionThree()
+            {
+                question.Text = "Which country has a population closest to 325 million people?";
+                b1.Text = "Pakistan";
+                b2.Text = "United Kingdom";
+                b3.Text = "Brazil";
+                b4.Text = "United States";
 
-            //if (questionNumber == 1)
-            //{
-            //    questionOne();
-            //}
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(3);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(3);
+                };
+                b3.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b3.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(3);
+                };
+                b4.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(3);
+                };
+            }
+            void questionFour()
+            {
+                b3.Visibility = Android.Views.ViewStates.Invisible;
+                b4.Visibility = Android.Views.ViewStates.Invisible;
+                question.Text = "Which country has the largest number of citizens?";
+                b1.Text = "Australia";
+                b2.Text = "Canada";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(4);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(4);
+                };
+            }
+            void questionFive()
+            {
+                b3.Visibility = Android.Views.ViewStates.Invisible;
+                b4.Visibility = Android.Views.ViewStates.Invisible;
+                question.Text = "Which country has more citizens?";
+                b1.Text = "South Korea";
+                b2.Text = "North Korea";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(5);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(5);
+                };
+            }
+            void questionSix()
+            {
+                question.Text = "Which of these countries have the most people?";
+                b1.Text = "Germany";
+                b2.Text = "France";
+                b3.Text = "Japan";
+                b4.Text = "Italy";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b3.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(6);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b3.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(6);
+                };
+                b3.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b3.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(6);
+                };
+                b4.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b3.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b4.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(6);
+                };
+            }
+            void questionSeven()
+            {
+                question.Text = "Which of these countries have the most people?";
+                b1.Text = "Japan";
+                b2.Text = "Brazil";
+                b3.Text = "Mexico";
+                b4.Text = "Russia";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(7);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(7);
+                };
+                b3.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b3.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(7);
+                };
+                b4.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b2.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b4.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(7);
+                };
+            }
+            void questionEight()
+            {
+                question.Text = "Which of these countries have the least amount of people?";
+                b1.Text = "Sweden";
+                b2.Text = "Finland";
+                b3.Text = "Denmark";
+                b4.Text = "Norway";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.Red);
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(8);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b2.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(8);
+                };
+                b3.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b3.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(8);
+                };
+                b4.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b4.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(8);
+                };
+            }
+            void questionNine()
+            {
+                question.Text = "Which of these countries have the most people?";
+                b1.Text = "Nigeria";
+                b2.Text = "Mexico";
+                b3.Text = "Japan";
+                b4.Text = "Russia";
+
+                b1.Click += delegate
+                {
+                    Toast.MakeText(this, "Correct", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    disableButtons();
+                    numberCorrect++;
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(9);
+                };
+                b2.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b2.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(9);
+                };
+                b3.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b3.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(9);
+                };
+                b4.Click += delegate
+                {
+                    Toast.MakeText(this, "Incorrect", ToastLength.Short).Show();
+                    b1.SetBackgroundColor(Android.Graphics.Color.DarkGreen);
+                    b4.SetBackgroundColor(Android.Graphics.Color.Red);
+                    disableButtons();
+                    totalQuestions++;
+                    calculateScore();
+                    alreadyUsed.Add(9);
+                };
+            }
         }
     }
 }
